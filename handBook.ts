523e144f131021e7ab6e134fd514ff0af78db81a
ala-soltanzade <alasoltanzade356@gmail.com>
2024-07-25 04:11:57 -0400

    test

---
+++ handBook.ts
@@ -0,0 +1,45 @@
+// on 'message' and then calling it
+//message.toLowerCase();
+// Calling 'message'
+//message();
+const message = 'Hello world';
+
+message 
+//For some values, such as the primitives string and number, we can identify their type at runtime using the typeof operator. But for other things like functions, there’s no corresponding runtime mechanism to identify their types. 
+function fn(x){
+    return x.flip();
+}
+//only work if given an object with a callable flip property
+
+const user = {
+    name: "Ala",
+    age: 21,
+  };
+user.name;
+
+const announcement = "Hello World!";
+
+// We probably meant to write this...
+announcement.toLocaleLowerCase();
+announcement.toLocaleUpperCase();
+
+function flipCoin() {
+    // Meant to be Math.random()
+    return Math.random();
+//Operator '<' cannot be applied to types '() => number' and 'number'.
+  }
+
+const value = Math.random() < 0.5 ? "a" : "b";
+if (value !== "a") {
+
+} else if (value === "a") {  //value ==='b' is wrong
+//This comparison appears to be unintentional because the types '"a"' and '"b"' have no overlap.
+
+}
+
+import express from "express";
+const app = express();
+ 
+app.get("/", function (req, res) {
+  res.sendfile()
+}
