0400e0d29cd628c2cd66bb3647fcba25b615b51b
alasoltanzade <alasoltanzade356@gmail.com>
2024-09-01 02:49:37 -0400

    Namespaces and Modules

--- reference7.ts
+++ reference7.ts
@@ -136,3 +136,31 @@ declare namespace D3 {
     }
 }
 declare var d3: D3.Base;
+
+
+
+
+
+//Namespaces and Modules
+// In a .d.ts file or .ts file that is not a module:
+// declare module "SomeModule" {
+//     export function fn(): string;
+// }
+//myOtherModule.ts
+//<reference path="myModules.d.ts" />
+//import * as m from "SomeModule";
+
+//shapes.ts
+export namespace Shapess {
+    export class Triangle {
+        /* ... */
+    }
+    export class Square {
+        /* ... */
+    }
+}
+
+//shapeConsumer.ts
+// import * as shapes from "./shapes";
+// let t = new shapes.Shapes.Triangle(); // shapes.Shapes?
+
